72ada97f1bc950db20f2ea28f17213b5c18a4bd0
Ismael Jaras <is.jaras@gmail.com>
2025-02-06 17:35:03 -0300

    include custom dash

--- my_keyboard/keymap.c
+++ my_keyboard/keymap.c
@@ -59,10 +59,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             case CUSTOM_DASH:
                 if (get_mods() & MOD_MASK_SHIFT) {
                     unregister_mods(MOD_MASK_SHIFT);
-                    tap_code16(KC_MINS);
-                    register_mods(MOD_MASK_SHIFT);
+                    tap_code16(KC_SLSH);
                 } else {
-                    tap_code16(KC_UNDS);
+                    register_mods(MOD_MASK_SHIFT);
+                    tap_code16(KC_SLSH);
+                    unregister_mods(MOD_MASK_SHIFT);
                 }
                 return false;
         }
@@ -75,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         CUSTOM_PIPE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_EQL,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-        KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE, MS_WHLD, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+        KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MUTE, MS_WHLD, KC_N, KC_M, KC_COMM, KC_DOT, CUSTOM_DASH, KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_SPC, KC_ENT, MO(2), KC_RALT, KC_PSCR, KC_RCTL
     ),
 
